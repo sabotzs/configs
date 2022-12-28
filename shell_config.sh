d5ca8a0a882333fe9edd7440ee6f0cd03d111e9c
Georgi Kuklev <84275478+sabotzs@users.noreply.github.com>
2022-12-28 17:50:02 +0200

    Add oh-my-zsh custom config

--- shell_config.sh
+++ shell_config.sh
@@ -1,29 +1,86 @@
-# Customize the terminal prompt
-function parse_git_branch() {
-	local branch=$(git branch 2> /dev/null | grep -e '^*' | cut -d ' ' -f 2)
-	local result=' '
-	if [[ -n $branch ]]; then
-		result=" ($branch)"
-	fi
-	echo $result
-}
-setopt PROMPT_SUBST
-export PROMPT='%B%F{22}%2~%f%b%F{196}$(parse_git_branch)%f$ '
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
 
-# Add colors for listed files
-export CLICOLOR=1
-export LSCOLORS=exgxdxfxcxegedabagacad
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="half-life"
 
-# Aliases
-alias ll='ls -l'
-alias la='ls -A'
-alias l='ls -CF'
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
 
-# Add executables
-if command -v pyenv 1>/dev/null 2>&1; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# You can also set it to another string to have that shown instead of the default red dots.
+# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
+# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
+COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+	colored-man-pages
+	command-not-found
+	rust
+	swiftpm
+	web-search
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+export EDITOR='nvim'
+
+if command -v pyenv 1> /dev/null 2>&1; then
+	export PYENV_ROOT="$HOME/.pyenv"
+	export PATH="$PYENV_ROOT/bin:$PATH"
+	eval "$(pyenv init --path)"
+	eval "$(pyenv init -)"
 fi
 export PATH="~/.local/bin:$PATH"
+
+alias la='ls -A'
+alias ll='ls -l'
+alias l='ls -CF'
+alias lt='ls -ltFh'
+
+alias grep='grep --color'
+
+alias ff='find . -type f -name'
+alias fd='find . -type d -name'
+
+alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc'
+
+alias gs='git status'
+alias ga='git add'
+alias gb='git branch'
+alias gl='git log'
+alias gst='git stash'
+
+alias gc='git commit'
+alias gcm='git commit -m'
+
+alias gd='git diff'
+alias gds='git diff --staged'
+
+alias gr='git restore'
+alias grs='git restore --staged'
